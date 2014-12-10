d6225b3d9b82b0060a67ec7675002551f782d7a7
Marc Abramowitz <marc@marc-abramowitz.com>
2014-12-09 22:28:34 -0800

    Add tests for RST rendering

---
+++ tests/fixtures/test_rst_008.rst
@@ -0,0 +1,22 @@
+Here is some Python code for a ``Dog``:
+
+.. code-block:: python
+
+    class Dog(Animal):
+        def __init__(self, name):
+            self.name = name
+
+        def make_sound(self):
+            print('Ruff!')
+
+    dog = Dog('Fido')
+
+and then here is some bash:
+
+.. code-block:: bash
+
+    if [ "$1" = "--help" ]; then
+        echo "OK"
+    fi
+
+or click `SurveyMonkey <http://www.surveymonkey.com>`_
---
+++ tests/test_rst.py
@@ -0,0 +1,55 @@
+import codecs
+import os
+
+from readme.rst import render
+
+
+def test_rst_001():
+    assert render('Hello') == ('<p>Hello</p>\n', True)
+
+
+def test_rst_002():
+    assert render('http://mymalicioussite.com/') == (
+        ('<p><a rel="nofollow" href="http://mymalicioussite.com/">' +
+         'http://mymalicioussite.com/</a></p>\n'),
+        True)
+
+
+def test_rst_003():
+    _do_test_with_files('test_rst_003')
+
+
+def test_rst_004():
+    _do_test_with_files('test_rst_004')
+
+
+def test_rst_005():
+    _do_test_with_files('test_rst_005')
+
+
+def test_rst_006():
+    _do_test_with_files('test_rst_006')
+
+
+def test_rst_007():
+    _do_test_with_files('test_rst_007', expected_rendered=False)
+
+
+def test_rst_008():
+    _do_test_with_files('test_rst_008')
+
+
+def _do_test_with_files(test_name, expected_rendered=True):
+    rst_markup = read('{0}.rst'.format(test_name))
+    expected_html = read('{0}.html'.format(test_name))
+
+    out, rendered = render(rst_markup)
+
+    assert rendered == expected_rendered
+    assert out == expected_html
+
+
+def read(fn):
+    path = os.path.join(os.path.dirname(__file__), 'fixtures', fn)
+    with codecs.open(path, encoding='utf-8') as f:
+        return f.read()
