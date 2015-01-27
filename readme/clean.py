792c5f84600774290d9154f38623e9ac5eaece33
Jeff Forcier <jeff@bitprophet.org>
2015-01-27 15:10:04 -0800

    Add HR tags to allowed tags
    This way I can have nice little line breaks in my ReST :D all hail @dstufft

--- readme/clean.py
+++ readme/clean.py
@@ -23,7 +23,7 @@ ALLOWED_TAGS = [
 
     # Custom Additions
     "br", "cite", "col", "colgroup", "dd", "div", "dl", "dt", "h1", "h2", "h3",
-    "h4", "h5", "h6", "img", "p", "pre", "span", "table", "tbody", "td", "th",
+    "h4", "h5", "h6", "hr", "img", "p", "pre", "span", "table", "tbody", "td", "th",
     "thead", "tr", "tt",
 ]
 
