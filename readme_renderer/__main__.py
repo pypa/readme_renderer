51f8eeced9d1c718999877da378a91f599cbb15b
Remi Rampin <remirampin@gmail.com>
2018-09-24 12:57:11 -0400

    Use argparse.FileType

--- readme_renderer/__main__.py
+++ readme_renderer/__main__.py
@@ -8,16 +8,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Renders a .rst README to HTML",
     )
-    parser.add_argument('input', help="Input README file")
-    parser.add_argument('-o', '--output', help="Output file (default: stdout)")
+    parser.add_argument('input', help="Input README file",
+                        type=argparse.FileType('r'))
+    parser.add_argument('-o', '--output', help="Output file (default: stdout)",
+                        type=argparse.FileType('w'), default='-')
     args = parser.parse_args()
-    if args.output:
-        output_file = open(args.output, 'w')
-    else:
-        output_file = sys.stdout
-    input_file = open(args.input)
 
-    rendered = render(input_file.read(), stream=sys.stderr)
+    rendered = render(args.input.read(), stream=sys.stderr)
     if rendered is None:
         sys.exit(1)
-    print(rendered, file=output_file)
+    print(rendered, file=args.output)
