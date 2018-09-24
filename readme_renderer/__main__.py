7c494c9216247d4480f2b293947b75947b1fcb01
Remi Rampin <remirampin@gmail.com>
2018-09-24 12:32:47 -0400

    Use `if __name__` and argparse

--- readme_renderer/__main__.py
+++ readme_renderer/__main__.py
@@ -1,14 +1,23 @@
 from __future__ import absolute_import, print_function
+import argparse
 from readme_renderer.rst import render
 import sys
 
 
-if len(sys.argv) == 2:
-    with open(sys.argv[1]) as fp:
-        out = render(fp.read(), stream=sys.stderr)
-        if out is not None:
-            print(out)
-        else:
-            sys.exit(1)
-else:
-    print("Syntax: python -m readme_renderer <file.rst>", file=sys.stderr)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Renders a .rst README to HTML",
+    )
+    parser.add_argument('input', help="Input README file")
+    parser.add_argument('-o', '--output', help="Output file (default: stdout)")
+    args = parser.parse_args()
+    if args.output:
+        output_file = open(args.output, 'w')
+    else:
+        output_file = sys.stdout
+    input_file = open(args.input)
+
+    rendered = render(input_file.read(), stream=sys.stderr)
+    if rendered is None:
+        sys.exit(1)
+    print(rendered, file=output_file)
