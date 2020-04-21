pypa/readme_renderer

commit 9155cfe032817f433119f41018f8ad567e4a55e6

Unify handling of SVG and other images in RST. (#113)
ReadMeHTMLTranslator was overriden to not to render object tags for SVG.
Unfortunately it produced different results than for PNG or other
image types.

With this commit, HTML code for SVG images will be same as for other
images. It means that it won't have width, height or align attributes.
But it will be better to address this issue for all images types at once.

readme_renderer/rst.py
@@ -14,7 +14,6 @@
 from __future__ import absolute_import, division, print_function
 
 import io
-import os.path
 
 from docutils.core import publish_parts
 from docutils.writers.html4css1 import HTMLTranslator, Writer
@@ -25,26 +24,8 @@ from .clean import clean
 
 class ReadMeHTMLTranslator(HTMLTranslator):
 
-    def depart_image(self, node):
-        uri = node["uri"]
-        ext = os.path.splitext(uri)[1].lower()
-        # we need to swap RST's use of `object` with `img` tags
-        # see http://git.io/5me3dA
-        if ext == ".svg":
-            # preserve essential attributes
-            atts = {}
-            for attribute, value in node.attributes.items():
-                # we have no time for empty values
-                if value:
-                    if attribute == "uri":
-                        atts["src"] = value
-                    else:
-                        atts[attribute] = value
-
-            # toss off `object` tag
-            self.body.pop()
-            # add on `img` with attributes
-            self.body.append(self.starttag(node, "img", **atts))
+    # Overrides base class not to output `<object>` tag for SVG images.
+    object_image_types = {}
 
 
 SETTINGS = {

tests/fixtures/test_rst_png_attrs.html
@@ -1,0 +1,1 @@
+<img alt="alternate text" src="https://example.com/badge.png">

tests/fixtures/test_rst_svg_attrs.html
@@ -1,0 +1,1 @@
+<img alt="alternate text" src="https://example.com/badge.svg">

tests/fixtures/test_rst_img_attrs.html
@@ -1,1 +1,0 @@
-<img align="right" alt="alternate text" height="100px" src="https://example.com/badge.svg" width="25.0%">

tests/fixtures/test_rst_png.html
@@ -1,0 +1,1 @@
+<img alt="https://example.com/badge.png" src="https://example.com/badge.png">

tests/fixtures/test_rst_svg.html
@@ -1,1 +1,1 @@
-<img src="https://example.com/badge.svg">
+<img alt="https://example.com/badge.svg" src="https://example.com/badge.svg">

commit 40b1460d067a1abec8744c251c779f26693aa8b0

Fix regression for image height/width
This fixes a regression introduced in #113 where height/width attributes
were removed from images defined in reStructuredText.

More discussion here: https://github.com/pypa/readme_renderer/pull/113#discussion_r206366539

readme_renderer/rst.py
@@ -27,6 +27,18 @@ class ReadMeHTMLTranslator(HTMLTranslator):
     # Overrides base class not to output `<object>` tag for SVG images.
     object_image_types = {}
 
+    def emptytag(self, node, tagname, suffix="\n", **attributes):
+        """Override this to add back the width/height attributes."""
+        if tagname == "img":
+            if "width" in node:
+                attributes["width"] = node["width"]
+            if "height" in node:
+                attributes["height"] = node["height"]
+
+        return super(ReadMeHTMLTranslator, self).emptytag(
+            node, tagname, suffix, **attributes
+        )
+
 
 SETTINGS = {
     # Cloaking email addresses provides a small amount of additional

tests/fixtures/test_rst_png_attrs.html
@@ -1,1 +1,1 @@
-<img alt="alternate text" class="align-right" src="https://example.com/badge.png">
+<img alt="alternate text" class="align-right" height="100px" src="https://example.com/badge.png" width="25.0%">

tests/fixtures/test_rst_svg_attrs.html
@@ -1,1 +1,1 @@
-<img alt="alternate text" class="align-right" src="https://example.com/badge.svg">
+<img alt="alternate text" class="align-right" height="100px" src="https://example.com/badge.svg" width="25.0%">