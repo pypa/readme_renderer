665d5509af12f8de12e65d4e05d20b5b26c67879
Mike Fiedler <miketheman@gmail.com>
2022-10-31 16:25:55 -0400

    fix: allow HTML5 figure tag through cleaner (#265)

--- readme_renderer/clean.py
+++ readme_renderer/clean.py
@@ -30,7 +30,7 @@ ALLOWED_TAGS = [
     "br", "caption", "cite", "col", "colgroup", "dd", "del", "details", "div",
     "dl", "dt", "h1", "h2", "h3", "h4", "h5", "h6", "hr", "img", "p", "pre",
     "span", "sub", "summary", "sup", "table", "tbody", "td", "th", "thead",
-    "tr", "tt", "kbd", "var", "input", "section", "aside", "nav", "s",
+    "tr", "tt", "kbd", "var", "input", "section", "aside", "nav", "s", "figure",
 ]
 
 ALLOWED_ATTRIBUTES = {
@@ -64,6 +64,7 @@ ALLOWED_ATTRIBUTES = {
     "dt": ["class"],
     "ul": ["class"],
     "nav": ["class"],
+    "figure": ["class"],
 }
 
 
