e1315844c23f46271f5edfa55ae76dcf6855bc62
Mike Fiedler <miketheman@gmail.com>
2023-05-31 15:33:00 +0000

    chore: bump version for development
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- readme_renderer/__about__.py
+++ readme_renderer/__about__.py
@@ -29,7 +29,7 @@ __summary__ = (
 )
 __uri__ = "https://github.com/pypa/readme_renderer"
 
-__version__ = "37.3"
+__version__ = "37.4.dev"
 
 __author__ = "The Python Packaging Authority"
 __email__ = "admin@mail.pypi.org"
