7e7c6aac1f0802f8bc6029452c3f9e5765ae4a99
Mike Fiedler <miketheman@gmail.com>
2023-09-01 20:59:30 +0000

    test: update fixtures
    nh3 is both tigher on removing truly scary code, and looser on
    escaping already-encapsulated quotations and control characters.
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -2,4 +2,6 @@ from readme_renderer.clean import clean
 
 
 def test_invalid_link():
-    assert clean('<a href="http://exam](ple.com">foo</a>') == "<a>foo</a>"
+    assert clean(
+        '<a href="http://exam](ple.com">foo</a>'
+    ) == '<a rel="nofollow">foo</a>'
