f3a0083464d94f572c851721d5db7762b90f0d27
Mike Fiedler <miketheman@gmail.com>
2023-09-05 18:51:53 +0000

    update checkbox fixture
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- readme_renderer/clean.py
+++ readme_renderer/clean.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List, Optional, Set
+from typing import Dict, Optional, Set
 
 import nh3
 
