1a56316c6e68270f26f188a50ce83352c936b9d1
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2026-06-02 20:10:39 +0200

    Fix ignored allowlists on `clean()`

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -5,3 +5,17 @@ def test_invalid_link():
     assert clean(
         '<a href="http://exam](ple.com">foo</a>'
     ) == '<a rel="nofollow">foo</a>'
+
+
+def test_custom_tags_are_respected():
+    assert clean(
+        "<p>plain text</p><br><strong>bold text</strong>",
+        tags={"br"},
+    ) == "plain text<br>bold text"
+
+
+def test_custom_attributes_are_respected():
+    assert clean(
+        '<img src="https://example.com/image.png" alt="image" width="100">',
+        attributes={"img": {"alt"}},
+    ) == '<img alt="image">'
